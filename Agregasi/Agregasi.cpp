d9fca8e429907afcee27b030afe8cd794070556a
vararais <varadigmaisnann@gmail.com>
2024-05-21 22:15:15 +0800

    Membuat Main

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
 
 int main()
 {
-    
+    ibu* varIbu = new ibu("suharti ayam goreng");
+    ibu* varIbu2 = new ibu("niki ngekost cibaduyut");
+    anak* varAnak1= new anak("bodat ngajoak kieu");
+    anak* varAnak2= new anak("siti karbu supra");
+    anak* varAnak3 = new anak("rozak kopling cbr");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu->tambahAnak(varAnak3);
+    varIbu->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+
 }
 
