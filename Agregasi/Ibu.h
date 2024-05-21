9444b2551a03bd2349bf2626ec523e9fe3cff852
vararais <varadigmaisnann@gmail.com>
2024-05-21 21:52:59 +0700

    Meralat Code

--- Agregasi/Ibu.h
+++ Agregasi/Ibu.h
@@ -1,16 +1,16 @@
 #ifndef IBU_H
-#define IBU_H
-#include <vector>
+#define IBU_H 
+#include<vector> 
 
 class ibu {
 public:
 	string nama;
-	vector<anak*> daftar_anak;
+	vector<anak* > daftar_anak;
 
 	ibu(string pNama) :nama(pNama) {
 		cout << "Ibu \"" << nama << "\" ada\n";
 	}
-    ~ibu(string pNama) :nama(pNama) {
+	~ibu() {
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
 	void tambahAnak(anak*);
@@ -21,9 +21,8 @@ void ibu::tambahAnak(anak* pAnak) {
 }
 void ibu::cetakAnak() {
 	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
-	
-	for (int i = 0; i < daftar_anak.size(); i++) {
-		cout << daftar_anak[i]->nama << endl;
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
 	}
 	cout << endl;
 }
